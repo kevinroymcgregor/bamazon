8dbbb60a09cf77f8690d8eb362b46f0ab32e8117
kevinroymcgregor <kevin.mcgregor85204@gmail.com>
2019-08-03 21:39:31 -0700

    Established connection and return basic results

--- SQLscripts/Setup.sql
+++ SQLscripts/Setup.sql
@@ -2,6 +2,22 @@ CREATE DATABASE bamazon;
 USE bamazon;
 CREATE TABLE products 
 	(
-		item_id INT auto_increment,
-        
+		item_id INT auto_increment NOT NULL,
+        product_name VARCHAR(50) NOT NULL UNIQUE,
+        department_name VARCHAR(50) NOT NULL,
+        price DOUBLE NOT NULL,
+        stock_quantity INT NOT NULL,
+        PRIMARY KEY (item_id)
 	);
+INSERT INTO products 
+	(
+		product_name, department_name, price, stock_quantity
+	)
+VALUES ("Star Wars Blu-Ray", "Movies", 20.00, 15),
+	("Empire Strikes Back Blu-Ray", "Movies", 19.95, 17),
+    ("Return of the Jedi Blu-Ray", "Movies", 21.98, 12),
+    ("Luke Skywalker Action Figure", "Toys", 12.00, 10),
+    ("Princess Leia Action Figure", "Toys", 15.00, 11),
+    ("Thrawn", "Books", 17.00, 15),
+    ("Thrawn: Alliances", "Books", 17.00, 15),
+    ("Thrawn: Treason", "Books", 12.00, 10);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,37 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 
+const connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "root",
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    //   console.log("connected as id " + connection.threadId + "\n");
+    //   createSong();
+    displayProducts();
+});
+
+const displayProducts = () => 
+    {
+        connection.query
+        (
+            `SELECT * FROM products`, 
+            function(err, results)
+                {
+                    if(err) throw err;
+                    console.log(results[0]);
+                    connection.end();
+                }
+        )
+    }
